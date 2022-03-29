1286dbeb57532908bba35d75e5e80c1f1096cacd
HassanAhmedBaig <76657818+HassanAhmedBaig@users.noreply.github.com>
2022-03-29 17:38:01 +0500

    Add files via upload

---
+++ module b assignment/app.js
@@ -0,0 +1,29 @@
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$"QUESTION#1"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+// Task to convert/return an object keys into a new array
+
+
+// let obj = {
+//     name: "HASSAN",
+//     password: 3322447700
+// };
+
+// let objkeys = Object.keys(obj);
+// console.log(objkeys);
+
+
+
+// ###########################"QUESTION#2"########################################
+
+// Task to convert/return an object values into a new array
+
+
+
+let obj = {
+    name: "HASSAN",
+    password: 3322447700
+};
+
+let objkeys = Object.values(obj);
+console.log(objkeys);
